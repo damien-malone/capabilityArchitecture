a6d1a86afc16f835c2c7ba73393ed754dae74dcc
damien-malone <damien_malone@yahoo.ie>
2018-06-23 23:21:38 +0100

    refactor main screen

--- Capability_Architecture/src/ribbonbardemo/Driver.java
+++ Capability_Architecture/src/ribbonbardemo/Driver.java
@@ -4,34 +4,33 @@ import javafx.application.Application;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import ui.common.StageHelper;
-import ui.common.StartScreen;
-import ui.layouts.ribbonBar.RibbonBar; 
+import ui.common.MainScreen;
+import ui.common.StartScreen; 
 
  
 /**
  * Driver. The Application starts here. 
  */ 
 public class Driver extends Application { 
- 
-    private Scene scene; 
-    private BorderPane layout; 
-     
+    
+    /**
+     * Application Entry Point. Program starts here. 
+     * @param args  
+     */ 
+    public static void main(String[] args) { 
+        launch(); 
+    } 
+	
     /**
      * start. Build UI and show here. 
      * @param stage  
      */ 
     @Override 
-    public void start(Stage stage) { 
-    	
-    	
+    public void start(Stage stage) {     	
     	StartScreen startScreen = new StartScreen(stage);
+    	MainScreen mainScreen = new MainScreen(new Stage());
+    	
     	startScreen.showPopup();
 
     	Task<Void> sleeper = new Task<Void>() {
@@ -48,72 +47,11 @@ public class Driver extends Application {
             @Override
             public void handle(WorkerStateEvent event) {
             	startScreen.hidePopup();
-            	showMainStage(new Stage());
+            	mainScreen.showMainStage();
             }
         });
         new Thread(sleeper).start();
     	
     }
-    
-    
-    public void showMainStage(Stage stage)
-    {
-    	StageHelper.setIcon(stage,this.getClass());
-    	stage.initStyle(StageStyle.DECORATED);
-        //Initalize layout 
-        layout = new BorderPane(); 
-        layout.setId("app"); 
-         
-        //Set BorderPane Regions 
-        RibbonBar r = new RibbonBar(); 
-        layout.setTop(r.get()); 
-         
-        //Set Scene properties. 
-        setSceneProperties(); 
-         
-        //Set a few properties of our Application Window 
-        stage.setScene(scene); 
-        stage.setTitle("Ribbon Menu Demo"); 
-        stage.show(); 
-    } 
-     
-    /**
-     * Application Entry Point. Program starts here. 
-     * @param args  
-     */ 
-    public static void main(String[] args) { 
-        launch(); 
-    } 
-     
-    /**
-     * setSceneProperties. This method sets the app to almost full size. It also 
-     * is where CSS style sheet is attached. 
-     */ 
-    private void setSceneProperties() 
-    { 
-        //The percentage values are used as multipliers for screen width/height. 
-        double percentageWidth = 0.98; 
-        double percentageHeight = 0.90; 
-         
-        //Calculate the width / height of screen. 
-        Rectangle2D screenSize = Screen.getPrimary().getBounds(); 
-        percentageWidth *= screenSize.getWidth(); 
-        percentageHeight *= screenSize.getHeight(); 
-                    
-        //Create a scene object. Pass in the layout and set 
-        //the dimensions to 98% of screen width & 90% screen height. 
-        this.scene = new Scene(layout, percentageWidth, percentageHeight); 
-         
-        //Add CSS Style Sheet (located in same package as this class). 
-        String css = this.getClass().getResource("App.css").toExternalForm(); 
-         
-        //Add CSS for Tabs. 
-        String tabsURL = "/ui/layouts/ribbonBar/tabs/tabs.css"; 
-        String tabsCSS = this.getClass().getResource(tabsURL).toExternalForm(); 
-         
-        scene.getStylesheets().addAll(css, tabsCSS); 
- 
-    } 
-    
             
 }
---
+++ Capability_Architecture/src/ui/common/MainScreen.java
@@ -0,0 +1,74 @@
+package ui.common;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import ui.layouts.ribbonBar.RibbonBar;
+
+public class MainScreen {
+    private Scene scene; 
+    private BorderPane layout; 
+    Stage stage;
+    
+    public MainScreen(Stage stage)
+    {
+    	this.stage = stage;
+    }
+    
+    
+	public void showMainStage()
+    {
+    	StageHelper.setIcon(stage,this.getClass());
+    	stage.initStyle(StageStyle.DECORATED);
+        //Initalize layout 
+        layout = new BorderPane(); 
+        layout.setId("app"); 
+         
+        //Set BorderPane Regions 
+        RibbonBar r = new RibbonBar(); 
+        layout.setTop(r.get()); 
+         
+        //Set Scene properties. 
+        setSceneProperties(); 
+         
+        //Set a few properties of our Application Window 
+        stage.setScene(scene); 
+        stage.setTitle("Ribbon Menu Demo"); 
+        stage.show(); 
+    } 
+	
+    /**
+     * setSceneProperties. This method sets the app to almost full size. It also 
+     * is where CSS style sheet is attached. 
+     */ 
+    private void setSceneProperties() 
+    { 
+        //The percentage values are used as multipliers for screen width/height. 
+        double percentageWidth = 0.98; 
+        double percentageHeight = 0.90; 
+         
+        //Calculate the width / height of screen. 
+        Rectangle2D screenSize = Screen.getPrimary().getBounds(); 
+        percentageWidth *= screenSize.getWidth(); 
+        percentageHeight *= screenSize.getHeight(); 
+                    
+        //Create a scene object. Pass in the layout and set 
+        //the dimensions to 98% of screen width & 90% screen height. 
+        this.scene = new Scene(layout, percentageWidth, percentageHeight); 
+         
+        //Add CSS Style Sheet (located in same package as this class). 
+        //String css = this.getClass().getResource("App.css").toExternalForm(); 
+         
+        //Add CSS for Tabs. 
+        String tabsURL = "/ui/layouts/ribbonBar/tabs/tabs.css"; 
+        //String tabsCSS = this.getClass().getResource(tabsURL).toExternalForm(); 
+         
+        //scene.getStylesheets().addAll(css, tabsCSS); 
+ 
+    } 
+	
+	
+}
